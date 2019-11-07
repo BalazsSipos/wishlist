02edfb6962aba3a10053d9e2e66208ef6cfb66c6
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-11-07 14:20:04 +0100

    EventService interface ready

---
+++ wishlist/wishlist/Services/EventService/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using wishlist.Models;
+
+namespace wishlist.Services.EventService
+{
+    public interface IEventService
+    {
+        Task<Event> GetEventByIdAsync(long eventId);
+    }
+}
