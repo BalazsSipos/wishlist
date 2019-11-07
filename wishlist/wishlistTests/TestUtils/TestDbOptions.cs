41c3de60b1d0c4321562c7ac46d7b1e5d44f8bb6
Gábor Trajtler <trajtlerg@gmail.com>
2019-11-07 22:20:24 +0100

    Add Test project with basic in-memory database fixture

---
+++ wishlist/wishlistTests/TestUtils/TestDbOptions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using wishlist;
+
+namespace wishlistTests.TestUtils
+{
+    class TestDbOptions
+    {
+        public static DbContextOptions<ApplicationDbContext> Get()
+        {
+            return new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "wishlist-testdb")
+                .Options;
+        }
+    }
+}
