1836379db84c23a70880ef2575868ae5f2de3e19
Gábor Trajtler <trajtlerg@gmail.com>
2019-11-07 13:30:42 +0100

    Add AccountController

--- wishlist/wishlist/Controllers/AccountController.cs
+++ wishlist/wishlist/Controllers/AccountController.cs
@@ -1,11 +1,58 @@
-﻿using System;
+﻿using wishlist.Models.RequestModels.Account;
+using wishlist.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace wishlist.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
+        private readonly IUserService userService;
+
+        public AccountController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await userService.Logout();
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login(string returnUrl)
+        {
+            LoginRequest loginRequest = await userService.CreateLoginRequest(returnUrl);
+            return View(loginRequest);
+        }
+
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl });
+            var properties = userService.ConfigureExternalAuthenticaticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
+
+        public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            try
+            {
+                returnUrl = returnUrl ?? Url.Content("~/");
+                string succeededReturnUrl = await userService.ExternalLoginCallbackAsync(returnUrl, remoteError);
+                return LocalRedirect(succeededReturnUrl);
+            }
+            catch (InvalidOperationException ex)
+            {
+                LoginRequest loginRequest = await userService.CreateLoginRequest(returnUrl);
+                ModelState.AddModelError(string.Empty, ex.Message);
+                return View("Login", loginRequest);
+            }
+        }
     }
 }
