0a6b83dd2d83611925a0d11212be4707c5c71bee
Balazs Sipos <balazs.sipos.hu@gmail.com>
2019-11-07 14:33:25 +0100

    Query event by id

---
+++ wishlist/wishlist/Services/EventService/EventService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using whishlist;
+using wishlist.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace wishlist.Services.EventService
+{
+    public class EventService : IEventService
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public EventService(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<Event> GetEventByIdAsync(long eventId)
+        {
+            var eventItem = await applicationDbContext.Events.Include(e => e.Gifts).Include(e => e.Invitations).FirstOrDefaultAsync(e => e.EventId == eventId);
+            if (eventItem == null)
+            {
+                return null;
+            }
+            return eventItem;
+        }
+    }
+}
